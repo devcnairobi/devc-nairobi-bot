48612ad78a5f5f8aeeffe87270f6ecfa64d350ac
Mitchel Kelonye <kelonyemitchel@gmail.com>
2017-04-12 15:45:03 +0300

    init logging using bunyan

--- bot/gh.js
+++ bot/gh.js
@@ -1,4 +1,5 @@
 import http from 'request';
+import { createLogger } from 'bunyan';
 
 const ORG = process.env.GH_ORG;
 const headers = {
@@ -7,10 +8,20 @@ const headers = {
   'Authorization': `token ${process.env.GH_OAUTH_TOKEN}`
 };
 
+const log = createLogger({
+  name: 'github',
+  stream: process.stdout,
+  level: 'info',
+});
+
 export function checkUsername(username){
   return new Promise(function(resolve, reject){
 
-    console.log(`checking username: ${username}`);
+    const logc = log.child({
+      username
+    });
+
+    logc.info(`checking username`);
 
     const opts = {
       url: `https://api.github.com/users/${username}`,
@@ -18,7 +29,11 @@ export function checkUsername(username){
     };
 
     http.get(opts, (err, res) => {
-      if (res.statusCode !== 200) return reject();
+      if (res.statusCode !== 200) {
+        logc.error('username check failed');
+        return reject();
+      }
+      logc.info('validated username!');
       resolve();
     });
 
@@ -29,7 +44,12 @@ export function checkUsername(username){
 export function addToOrg(username){
   return new Promise(function(resolve, reject){
 
-    console.log(`adding ${username} to ${ORG} org`);
+    const logc = log.child({
+      username,
+      org: ORG
+    });
+
+    logc.info(`adding user to org`);
 
     const opts = {
       url: `https://api.github.com/users/orgs/${ORG}/memberships/${username}`,
@@ -37,7 +57,11 @@ export function addToOrg(username){
     };
 
     http.put(opts, (err, res) => {
-      if (res.statusCode !== 200) return reject();
+      if (res.statusCode !== 200) {
+        logc.error('failed to add user to org');
+        return reject();
+      }
+      logc.info('added user to org!');
       resolve();
     });
 
