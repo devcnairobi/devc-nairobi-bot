b02e7f64eafb22673b50951a3a10acd6ba85a23e
Anthony Nandaa <profnandaa@gmail.com>
2017-04-15 02:34:51 +0300

    Revert "feat: auto add user to org [updated] (#21)" (#22)
    This reverts commit e6409a64071ce8bc725db332c2a0564c99210ad7.

--- bot/index.js
+++ bot/index.js
@@ -1,54 +1,40 @@
 const BootBot = require('bootbot');
+
 const replies = require('./replies');
 const User = require('./user');
 const db = require('../storage/firebase');
-const createLogger = require('bunyan').createLogger;
-
-const log = createLogger({
-  name: 'bot',
-  stream: process.stdout,
-  level: 'info',
-});
 
 const bot = new BootBot({
-  accessToken: process.env.PAGE_TOKEN,
-  verifyToken: process.env.VERIFY_TOKEN,
-  appSecret: process.env.APP_SECRET
+    accessToken: process.env.PAGE_TOKEN,
+    verifyToken: process.env.VERIFY_TOKEN,
+    appSecret: process.env.APP_SECRET
 });
 
 bot.on('error', (err) => {
-  log.error(err.message);
-});
-
-bot.on('message', (payload) => {
-  const ctx = {
-    sender: payload.sender.id,
-    message: payload.message.text,
-  };
-  log.child(ctx).info('message');
+    console.log(err.message)
 });
 
 bot.hear([/hi/i, /hello/i], (payload, chat) => {
-  chat.say(replies.default);
+    chat.say(replies.default);
 });
 
 bot.hear([/register/i, /sign[- ]?up/i], (payload, chat) => {
-  const psid = payload.sender.id; // Page scoped ID
-  chat.getUserProfile().then((user) => {
-    db.saveUser(psid, user, () => {
-      User.register(chat, (userPatch) => {
-        db.updateUser(psid, userPatch);
-      });
+    const psid = payload.sender.id; // Page scoped ID
+    chat.getUserProfile().then((user) => {
+        db.saveUser(psid, user, () => {
+            User.register(chat, (userPatch) => {
+                db.updateUser(psid, userPatch);
+            });
+        });
     });
-  });
 });
 
 bot.hear([/add [a-z ]* github/i], (payload, chat) => {
-  // phrase like 'add me to Github'
-  User.addToGithub(chat, (userPatch) => {
-    // okay to add redudant psid on the user object
-    db.updateUser(userPatch.psid, userPatch);
-  });
+    // phrase like 'add me to Github'
+    User.addToGithub(chat, (userPatch) => {
+        // okay to add redudant psid on the user object
+        db.updateUser(userPatch.psid, userPatch);
+    });
 });
 
 module.exports = bot;
--- bot/server.js
+++
@@ -1,4 +0,0 @@
-require('./load-env');
-const bot = require('./');
-
-module.exports = bot;
