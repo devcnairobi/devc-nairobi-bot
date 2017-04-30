8c5f7e2d846541c42863cff18855f34896815539
musale <musale@focusmobile.co>
2017-04-30 21:58:43 +0300

    check if user exists in fb

--- storage/firebase.js
+++ storage/firebase.js
@@ -1,4 +1,5 @@
 const fb = require('./config').firebase;
+const User = require('../bot/user');
 
 module.exports = {
   saveUser(psid, user, cb) {
@@ -16,5 +17,21 @@ module.exports = {
   updateUser(psid, user) {
     const userRef = fb.database().ref('/users/' + psid);
     userRef.update(user);
+  },
+
+  checkIfUserExists(psid, chat) {
+    const userRef = fb.database().ref(`/users/${psid}`);
+    userRef.once('value').then((snapshot) => {
+      const exists = (snapshot.val() !== null);
+      if (!exists) {
+        User.askIfToRegister(chat, (user) => {
+          this.saveUser(psid, user, () => {
+            User.register(chat, (userPatch) => {
+              this.updateUser(psid, userPatch);
+            });
+          });
+        });
+      }
+    });
   }
 };
