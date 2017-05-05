2d2c4604eb05081449e57a83ec0ff0f0c6482666
Anthony Nandaa <profnandaa@gmail.com>
2017-05-05 10:55:25 +0300

    feat: add crude RSVP for now

--- storage/firebase.js
+++ storage/firebase.js
@@ -1,6 +1,11 @@
 const fb = require('./config').firebase;
 const User = require('../bot/user');
 
+const timestamp = () => {
+  // return current_timestamp
+  return Math.round((new Date()).getTime());
+};
+
 module.exports = {
   saveUser(psid, user, cb) {
     // psid: page-scored ID
@@ -36,5 +41,24 @@ module.exports = {
         chat.say(`Welcome ${user.first_name}, you are already registered.`);
       }
     });
-  }
+  },
+
+  eventRSVP(psid, eventId, chat) {
+    const eventRef = fb.database().ref(`/RSVPs/${eventId}/${psid}`);
+    const userRef = fb.database().ref(`/users/${psid}`);
+    userRef.once('value').then((snapshot) => {
+      const user = snapshot.val();
+      if (user !== null) {
+        eventRef.set(user);
+        chat.say(`Thanks, you've been RSVP'd successfully for today's event :)`);
+      } else {
+        chat.say({
+          text: `Please register first.`,
+          quickReplies: [
+            { content_type: 'text', title: 'Register' },
+          ],
+        });
+      }
+    });
+  },
 };
